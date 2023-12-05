50e7d880be433b9d72b7ff9a784740d69e34b942
HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>
2023-12-05 17:03:17 +0400

    Update pixi.js
    Signed-off-by: HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>

--- pixi.js
+++ pixi.js
@@ -1,14 +1,15 @@
 const app= new PIXI.Application({
-    background: 'C3FE03',
+    background: 'F4FE03',
     width:800,
     height:400
 })
 document.body.appendChild(app.view)
+let arr1=[]
 let arr=[]
-let cond2=1
-let cond1=0
+let cond2=0
+let cond1=1
+let cond3=0
 let num=0
-let a=0
 class Plane{
     constructor(arg){
         this.sprite = PIXI.Sprite.from('https://cdn-icons-png.flaticon.com/512/2685/2685659.png');
@@ -18,17 +19,25 @@ class Plane{
         this.sprite.x=-this.sprite.width
         this.sprite.interactive = true; 
         this.sprite.buttonMode = true;
+        this.sprite.anchor.set(0)
         this.onClick=()=>{
-            if(cond2){
-                 a=1
-                 b=0
-            num=arr.indexOf(this)
-                app.ticker.add(()=>{
-                    if(this.sprite.y>0){
-                        cond2=0
-                     this.sprite.y-=2
-                   }else{cond2=1}
-            })
+          if(cond1){ 
+              d=0
+              num=0
+              cond3=1
+              let m=1
+          app.ticker.add(()=>{
+            if(m){
+              if(this.sprite.y>-100){
+                  cond1=0
+                  this.sprite.y-=3
+               }else{
+                m=0
+                cond1=1
+               }
+              }
+             num=arr.indexOf(this)
+          })    
          }
         }
         this.addChild=()=>{
@@ -36,61 +45,48 @@ class Plane{
             this.sprite.on('tap',this.onClick)
             app.stage.addChild(this.sprite)
         }
-        this.start=()=>{
-            this.sprite.x+=1.5
-            this.sprite.scale.y*=1.0005
-            this.sprite.scale.x*=1.0005
-        }
-        this.ticker=()=>{
-            app.ticker.add(()=>{
-                this.start()
-                if(this.sprite.x>app.view.width){
-                    app.stage.removeChild(this.sprite)
-                }
-                if(this.sprite.y==0){
-                    app.stage.removeChild(this.sprite)
-                }
-            })
-        }
     }
 }
+for(let i=0;i<20;i++){
+   let plane= new  Plane(i)
+   plane.addChild()
+   plane.sprite.x=-100
+   arr1.push(plane)
+}
 
-let result= new  Plane(arr.length)
-result.addChild()
-result.ticker()
-arr.push(result)
+arr.unshift(arr1.shift())
+arr[0].sprite.x=-100
+
 app.ticker.add(()=>{
-    if(arr[0].sprite.x>60){
-        num++
-        let result= new  Plane()
-        result.addChild()
-        result.ticker()
-        arr.unshift(result)
-        if(arr[arr.length-1].sprite.x>app.view.width){
-            arr.pop()
-        }
+  if(arr[0].sprite.x>=0){
+    arr.unshift(arr1.shift())
+    arr[0].sprite.x=-100
+  }
+  for(let i=0;i<arr.length;i++){
+    arr[i].sprite.scale.y*=1.0003
+    arr[i].sprite.scale.x*=1.0003
+    arr[i].sprite.x+=1
+    if(arr[i].sprite.x>=app.view.width){
+      let n=arr.pop()
+      n.sprite.scale.y=0.15
+      n.sprite.scale.x=0.15
+      n.sprite.y=150
+      arr1.push(n)
     }
+  }
 })
-let b=0
+
+ let d=0 
 app.ticker.add(()=>{
-    if(num==0){
-        num=1
+       if(num){
+        d+=3
+        if(d<100){
+             for(let i=0;i<num;i++){
+             arr[i].sprite.x+=3
+           }
+         }else{
+        num=0
+      }    
     }
-    if(a){
-     b+=2   
-     if(num<=arr.length-2){
-    if(b<arr[1].sprite.x-arr[0].sprite.x){
-      for(let i=0;i<num;i++){
-        arr[i].sprite.x+=2
-     }  
-    }
-   }else{
-    if(b<(arr[1].sprite.x-arr[0].sprite.x)/2){
-          for(let i=0;i<num;i++){
-            arr[i].sprite.x+=2
-          }  
-        }
-     }
-  } 
+
 })
- 
