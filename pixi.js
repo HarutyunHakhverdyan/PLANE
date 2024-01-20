acabdabcff1c3aa505eb03bbdcab38d5c9f31810
HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>
2024-01-20 19:30:53 +0400

    Update pixi.js
    Signed-off-by: HarutyunHakhverdyan <92093271+HarutyunHakhverdyan@users.noreply.github.com>

--- pixi.js
+++ pixi.js
@@ -1,102 +1,103 @@
 const app= new PIXI.Application({
-  background: 'FED300',
-  width:800,
-  height:400
-})
-document.body.appendChild(app.view)
-let arr1=[]
-let arr=[]
-let cond2=0
-let cond1=1
-let cond3=0
-let num=0
-class Plane{
-  constructor(arg){
-      this.sprite = PIXI.Sprite.from('https://cdn-icons-png.flaticon.com/512/2685/2685659.png');
-      this.sprite.scale.x *=0.15 ;
-      this.sprite.scale.y *= 0.15;
-      this.sprite.y=150
-      this.sprite.x=-this.sprite.width
-      this.sprite.interactive = true; 
-      this.sprite.buttonMode = true;
-      this.sprite.anchor.set(0)
-      this.onClick=()=>{
-           length=0
-        if(cond1){ 
-            d=0
-            num=0
-            cond3=1
-            let m=1
-        app.ticker.add(()=>{
-          if(m){
-            if(this.sprite.y>-150){
-                cond1=0
-                this.sprite.y-=3
-             }else{
-              m=0
-              cond1=1
-             }
-            }
-            length=this.sprite.width
-           num=arr.indexOf(this)
-        })    
-       }
-      }
-      this.addChild=()=>{
-          this.sprite.on('click', this.onClick) 
-          this.sprite.on('tap',this.onClick)
-          app.stage.addChild(this.sprite)
-      }
+    background: '2F719F',
+    width:1000,
+    height:400
+  })
+  document.body.appendChild(app.view)
+  let reservPlane=[];
+  let viewPlane=[]
+  let num=0
+  let condClickNum=false
+  let condSpeed=false
+  class Plane{
+    constructor(arg){
+        this.sprite = PIXI.Sprite.from('https://cdn-icons-png.flaticon.com/512/4998/4998191.png');
+        this.sprite.scale.x *=0.2 ;
+        this.sprite.scale.y *= 0.2;
+        this.sprite.y=150
+        this.sprite.x=-300
+        this.sprite.interactive = true; 
+        this.sprite.buttonMode = true;
+        this.sprite.anchor.set(0)
+        this.onClick=()=>{
+            if(!condSpeed&&!condClickNum){
+                condSpeed=true
+               condClickNum=true
+               num=viewPlane.indexOf(this) 
+            } 
+        }
+        this.addChild=()=>{
+            this.sprite.on('click', this.onClick) 
+            this.sprite.on('tap',this.onClick)
+            app.stage.addChild(this.sprite)
+        }
+     }
   }
-}
-for(let i=0;i<30;i++){
- let plane= new  Plane(i)
- plane.addChild()
- plane.sprite.x=-80
- arr1.push(plane)
-}
-
-arr.unshift(arr1.shift())
-arr[0].sprite.x=-100
-
-app.ticker.add(()=>{
-if(arr[0].sprite.x>=-1){
-   arr.unshift(arr1.shift())
-   arr[0].sprite.x=-80
-   arr[0].sprite.y=150  
- }
-for(let i=0;i<arr.length;i++){
-  arr[i].sprite.scale.y*=1.0015
-  arr[i].sprite.scale.x*=1.0015
-  arr[i].sprite.x+=1
-  arr[i].sprite.y-=0.1
-  arr[i].sprite.x*=1.0015
-  if(arr[i].sprite.x>=app.view.width){
-    let n=arr.pop()
-    n.sprite.scale.y=0.15
-    n.sprite.scale.x=0.15
-    n.sprite.y=-150
-    arr1.push(n)
+  
+  function reserv(){
+    for(let i=0;i<40;i++){
+      let plane = new Plane()
+      plane.addChild()
+      reservPlane.push(plane)
+    }
+    viewPlane.unshift(reservPlane.shift())
   }
-}
-})
-let d=0 
-app.ticker.add(()=>{
-     if(num){
-      d+=3
-      if(d<length+10){
+  reserv()
 
-           for(let i=0;i<num;i++){
-             
-           arr[i].sprite.x+=3
-           arr[i].sprite.scale.y*=1.0015 
-           arr[i].sprite.y-=0.1
-          
-         }
-       }else{
-      num=0
-    }    
+  function travelIncrement(){
+    if(viewPlane[0].sprite.x>-200){
+        
+        let newPlane=reservPlane.shift()
+        newPlane.sprite.y=150
+        newPlane.sprite.x=-300
+        newPlane.sprite.scale.y=0.2
+        newPlane.sprite.scale.x=0.2
+        viewPlane.unshift(newPlane)
+        ++num
+     }
+    for(let i=0;i<viewPlane.length;i++){
+        console.log(viewPlane[viewPlane.length-1].sprite.x)
+        viewPlane[i].sprite.x+=2
+        viewPlane[i].sprite.scale.x*=1.0015
+        viewPlane[i].sprite.x*=1.0015
+        if(viewPlane[i].sprite.y>0){
+            viewPlane[i].sprite.scale.y*=1.0015
+            viewPlane[i].sprite.y/=1.0015
+        }
+        
+    }
+
+    if(viewPlane[viewPlane.length-1].sprite.x>app.view.width+500){
+        reservPlane.push(viewPlane.pop())
+    }
   }
-})
 
-
+  function crash(i){
+    console.log(viewPlane[i])
+   viewPlane[i].sprite.y-=4 
+   if(viewPlane[i].sprite.y<-200){
+    condClickNum=false
+   }
+  }
+  function speeding(num){
+    if(viewPlane[num].sprite.x>viewPlane[num-1].sprite.x){
+        for(let i=0;i<num;i++){
+            viewPlane[i].sprite.x+=4
+            viewPlane[i].sprite.scale.y*=1.003
+            viewPlane[i].sprite.scale.x*=1.003
+            viewPlane[i].sprite.x*=1.003
+            viewPlane[i].sprite.y/=1.003
+    }
+    }else{
+        condSpeed=false
+    }
+  }
+ app.ticker.add(()=>{
+    travelIncrement()
+    if(condClickNum){
+       crash(num) 
+    }
+    if(condSpeed){
+      speeding(num)  
+    }
+ })
